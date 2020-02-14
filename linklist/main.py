4b1c03cc216d4a132de49f8bd0b0f5c49038654b
yashchandel0007 <yashchandel007@outlook.com>
2020-02-15 02:21:13 +0530

    simple linklist program created without any insertion funtion but contains traversal function

--- linklist/main.py
+++ linklist/main.py
@@ -1,1 +1,33 @@
-print ("linklist")
+print("linklist")
+
+
+class Node:
+    def __init__(self, data):
+        self._data = data
+        self.next_ = None
+
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+    def traversal(self):
+        temp = self.head
+        if temp == None:
+            print("The list is empty")
+        else:
+            while temp != None:
+                print(temp._data)
+                temp = temp.next_
+
+list1 = Linkedlist()
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+list1.head = n1
+list1.head.next_ = n2
+n2.next_ = n3
+n3.next_ = n4
+
+list1.traversal()
